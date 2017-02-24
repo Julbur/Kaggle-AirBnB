4d8ca661ab25d8f85213e4017f682f093faf4334
MGadonna <may-line.gadonna@agroparistech.fr>
2017-02-24 10:18:46 +0100

    Add files via upload

---
+++ Analyse_donnees.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 24 10:00:46 2017
+
+@author: May-line
+"""
+
+import pandas as pd
+
+#train=pd.read_csv('train_users_2.csv')
+#test=pd.read_csv('test_users.csv')
+
+#train.values[:,15].value_counts()
+histo=pd.value_counts(train.values[:,15])
+print(histo)
